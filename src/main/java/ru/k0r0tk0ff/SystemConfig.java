59c70ed4ab59206fba93c1368e7b7ce0bc27b2ec
Alexandr <korotkov34reg@gmail.com>
2018-10-01 16:06:45 +0300

    Create SystemConfig.java

---
+++ src/main/java/ru/k0r0tk0ff/SystemConfig.java
@@ -0,0 +1,32 @@
+package ru.k0r0tk0ff;
+
+public class SystemConfig implements SystemConfigMBean {
+    private int threadCount;
+    private String schemaName;
+
+    public SystemConfig(int numThreads, String schema){
+        this.threadCount=numThreads;
+        this.schemaName=schema;
+    }
+
+    public void setThreadCount(int noOfThreads) {
+        this.threadCount=noOfThreads;
+    }
+
+    public int getThreadCount() {
+        return this.threadCount;
+    }
+
+    public void setSchemaName(String schemaName) {
+        this.schemaName=schemaName;
+    }
+
+    public String getSchemaName() {
+        return this.schemaName;
+    }
+
+    public String doShowCountValue(){
+        return "No of Threads="+this.threadCount+" and DB Schema Name="+this.schemaName;
+    }
+
+}
